958aad7ef2b76f107c4a9a30b89e5f3778e82871
shital-meshram <74980959+shital-meshram@users.noreply.github.com>
2021-02-02 16:29:36 +0530

    FrontEnd and BackEnd Uploaded

---
+++ backend/src/main/java/com/app/JavaProjectEtohfaSpringBootRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaProjectEtohfaSpringBootRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaProjectEtohfaSpringBootRestApiApplication.class, args);
+	}
+
+}
